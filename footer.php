8ec6ac10b61aab82e2fc4dbe5497c1ffb722edbc
Pavel Rusakov <rusakov@ittest-team.ru>
2020-03-28 19:19:16 +0300

    Базовый перенос верски в php

---
+++ footer.php
@@ -0,0 +1,67 @@
+
+
+
+
+<footer class="footer">
+    <div class="footer padding_site">
+        <div class="featured_sale padding_footer">
+            <h3>FEATUDER SALE</h3>
+            <ul>
+                <li><a href="#" >Alexis Hudson</a></li>
+                <li><a href="#" >American Apparel</a></li>
+                <li><a href="#" >Ben Sherman</a></li>
+                <li><a href="#" >Big Buddha</a></li>
+                <li><a href="#" >Chanel</a></li>
+                <li><a href="#" >Christian Audigier</a></li>
+                <li><a href="#" >Coach</a></li>
+                <li><a href="#" >Cole Haan</a></li>
+            </ul>
+        </div>
+        <div class="mens_store padding_footer">
+            <h3>MENS STORE</h3>
+            <ul>
+                <li><a href="#" >Alexis Hudson</a></li>
+                <li><a href="#" >American Apparel</a></li>
+                <li><a href="#" >Ben Sherman</a></li>
+                <li><a href="#" >Big Buddha</a></li>
+                <li><a href="#" >Chanel</a></li>
+                <li><a href="#" >Christian Audigier</a></li>
+                <li><a href="#" >Coach</a></li>
+                <li><a href="#" >Cole Haan</a></li>
+            </ul>
+        </div>
+        <div class="womes_store padding_footer">
+            <h3>WOMEN STORE</h3>
+            <ul>
+                <li><a href="#" >Alexis Hudson</a></li>
+                <li><a href="#" >American Apparel</a></li>
+                <li><a href="#" >Ben Sherman</a></li>
+                <li><a href="#" >Big Buddha</a></li>
+                <li><a href="#" >Chanel</a></li>
+                <li><a href="#" >Christian Audigier</a></li>
+                <li><a href="#" >Coach</a></li>
+                <li><a href="#" >Cole Haan</a></li>
+            </ul>
+        </div>
+        <div class="quick_links padding_footer">
+            <h3>QUICK LINKS</h3>
+            <ul>
+                <li><a href="#" >Alexis Hudson</a></li>
+                <li><a href="#" >American Apparel</a></li>
+                <li><a href="#" >Ben Sherman</a></li>
+                <li><a href="#" >Big Buddha</a></li>
+                <li><a href="#" >Chanel</a></li>
+                <li><a href="#" >Christian Audigier</a></li>
+                <li><a href="#" >Coach</a></li>
+                <li><a href="#" >Cole Haan</a></li>
+            </ul>
+        </div>
+    </div>
+
+</footer>
+<script src="scripts/swiper.min.js"></script>
+<script src="scripts/jquery-3.1.1.js"></script>
+<script src="scripts/script.js"></script>
+
+</body>
+</html>
